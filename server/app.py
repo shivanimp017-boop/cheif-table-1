d4f458504eca651090c868af1aa3ec5ae6cef3ba
shivanimp017 <shivanimp017@gmail.com>
2026-04-12 12:23:14 +0530

    fix: add main() function to server/app.py

--- server/app.py
+++ server/app.py
@@ -4,5 +4,10 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app
 
-if __name__ == '__main__':
+
+def main():
     app.run(debug=False, host='0.0.0.0', port=5000)
+
+
+if __name__ == '__main__':
+    main()
